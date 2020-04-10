e3cbcdd6ab03324441c24d56ba2fd2f26ef72e92
gdj <gdeanda@inmegen.edu.mx>
2020-04-09 22:32:38 -0500

    advanced qPCR cdc

--- qPCR_cdc.R
+++ qPCR_cdc.R
@@ -24,13 +24,13 @@ my_deltaRN <- tidy_deltaRN(resultados) #read deltaRN from EDS file
 #define probes
 ########
 
-all_probes <- c("RP", "N1", "N2")
-names(all_probes) <- all_probes
+cdc_probes <- c("RP", "N1", "N2")
+names(cdc_probes) <- cdc_probes
 ################################################################################
 #Separate sample wells from QC wells 
 ################################################################################
-wells.ntc <- grep(pattern = "NTC", x = colnames(my_deltaRN))
-wells.ptc <- grep(pattern = "PTC", x = colnames(my_deltaRN))
+#wells.ntc <- grep(pattern = "NTC", x = colnames(my_deltaRN))
+#wells.ptc <- grep(pattern = "PTC", x = colnames(my_deltaRN))
 ################################################################################
 #Plate Quality Control
 ################################################################################
@@ -40,68 +40,42 @@ plate_curves <-
 pivot_deltaRN(my_deltaRN) %>% 
   plot_deltaRN.long()
 
-qc_results <- plate_qc(my_deltaRN)
+qc_results <- plate_qc(tdrn = my_deltaRN, 
+                       all_probes = cdc_probes)
 
 ################################################################################
 #Sample analysis
 ################################################################################
-test.df <-
-  my_deltaRN %>% 
-  select(!c(wells.ntc, wells.ptc), cycles) %>% 
-  pivot_deltaRN %>% 
-  separate(col = sample.id, sep = "_", into = c("well", 
-                                                "sample.label", 
-                                                "probe"), 
-           remove = F)
 
-test.samples <- unique(test.df$sample.label)
-names(test.samples) <- test.samples
 
-test.results <- 
-  lapply(test.samples, FUN = function(my_sample){
+test.results <- test.plate(tdrn = my_deltaRN, probes = cdc_probes)
     
-    sample_data <-
-      test.df %>% 
-      filter(sample.label == my_sample) 
-    
-    #we evaluate all probes
-    
-    lapply(X = all_probes, FUN = function(my_probe){
-      #we use a trycatch to get NAs for probes not measured in well
-      tryCatch(
-        {
-          #extract curve
-          the_curve     <- extract_curve(sample_data, probe == my_probe)
-          #extract threshold
-          
-          
-          
-          the_threshold <- get_threshold.rg(the_curve)
-          print(the_threshold)
-          #does the curve crosses the threshold?
-          #threshold for RP should be crossed at time 35
-          any(the_curve$value[1:40] > the_threshold)
-        },
-        error =function(cond){
-          message("well does not have that probe")
-          return(NA)
-        }
-      )
-    }) %>% bind_rows()
-  })%>% bind_rows(.id = "sample")
-
-test.results  
 ################################################################################
 #Plot preparation
 ################################################################################
 
+plots.qc <- plot.curves(tdrn = my_deltaRN, 
+                        probes = cdc_probes, 
+                        qc = T)
+
+plots.samples <- plot.curves(tdrn = my_deltaRN, 
+                        probes = cdc_probes, 
+                        qc = F)
 ################################################################################
 #Sample diagnostic
 ################################################################################
 
+test_diagnosis <- cdc_classification(test.results)
+
 ################################################################################
 #Write individual reports
 ################################################################################
+
+make_reports(plot_list = plots.qc, 
+             result_table = qc_results$qc.values, 
+             outdir = "results/otra_prueba/", 
+             qc = T)
+
 
 ################################################################################
 #Write QC output
