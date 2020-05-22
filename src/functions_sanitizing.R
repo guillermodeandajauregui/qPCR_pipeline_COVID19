832386c976228540e5dc5e1eb9a8de4b9f9acd56
gdj <gdeanda@inmegen.edu.mx>
2020-05-22 12:00:29 -0500

    added function to check that sample names have no special character

---
+++ src/functions_sanitizing.R
@@ -0,0 +1,8 @@
+################################################################################
+#
+#functions for qPCR analysis - COVID19 detection
+#Sanitizing inputs
+#by: INMEGEN Computational Genomics Dept
+#Guillermo de Anda Jauregui gdeanda@inmegen.edu.mx
+#
+################################################################################
