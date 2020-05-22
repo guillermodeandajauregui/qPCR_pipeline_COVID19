8d3d43bdc16b5b4092ab7ef1196b36bb9f6f9d3b
gdj <gdeanda@inmegen.edu.mx>
2020-05-22 13:01:39 -0500

    removed debug messages

--- src/functions_sanitizing.R
+++ src/functions_sanitizing.R
@@ -60,3 +60,46 @@ CheckNamesEDS <- function(eds){
   return(my_r)
   
 }
+
+### Check correct probes 
+
+CheckProbesEDS <- function(eds, my_probes){
+  #takes a path to an eds 
+  #extracts and checks that sample names are legible 
+  eds.file <- unz(description = eds, filename = "apldbio/sds/analysis_result.txt")
+  analysis_text <- read_lines(eds.file)
+  #find the lines with the Delta Rn
+  delta_rn.id <- which(str_detect(string = analysis_text, pattern = "Delta Rn"))
+  
+  ### name them with the info that is two rows before
+  
+  nomen <- 
+    analysis_text[delta_rn.id - 2] %>% 
+    lapply(FUN = function(i){
+      intermedio <- str_split(string = i, pattern = "\t")
+      intermedio <- unlist(intermedio, recursive = F)
+      df = data.frame(sample = intermedio[2], probe = intermedio[3])
+    }) 
+  
+  nomen = suppressWarnings(bind_rows(nomen))
+  
+  los_probes = 
+  nomen %>% 
+    group_by(sample) %>% 
+    group_map(~ unique(.x$probe))
+  
+  #check they are the same probes with jaccard
+  j_probes <-
+  lapply(los_probes, function(i){
+    
+    J = length(intersect(i, my_probes))/length(union(i, my_probes))
+    
+    return(J==1)
+  })
+  
+  #if all samples have all probes, TRUE
+  resultado = suppressWarnings(all(j_probes))
+  return(resultado)
+  
+}
+
